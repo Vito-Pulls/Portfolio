aedeacb913f91b2f9b17ed65d906bd078c950c71
Vi <victor.suarez@alu.ceacfp.es>
2026-03-19 06:16:23 +0100

    fix:rutas admin

--- config/rutas.php
+++ config/rutas.php
@@ -1,3 +1,4 @@
 <?php
 // Detecta automáticamente el subdirectorio del proyecto
 define('BASE_URL', rtrim(dirname($_SERVER['SCRIPT_NAME']), '/\\'));
+define('BASE_URL_ADMIN', rtrim(dirname(dirname($_SERVER['SCRIPT_NAME'])), '/\\'));
