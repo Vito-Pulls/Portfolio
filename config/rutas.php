2ea218393570f75973da13cb98221ccc0290822e
Vi <victor.suarez@alu.ceacfp.es>
2026-03-19 06:51:32 +0100

    fix:rutas y contrasenya

--- config/rutas.php
+++ config/rutas.php
@@ -1,4 +1,13 @@
 <?php
-// Detecta automáticamente el subdirectorio del proyecto
-define('BASE_URL', rtrim(dirname($_SERVER['SCRIPT_NAME']), '/\\'));
-define('BASE_URL_ADMIN', rtrim(dirname(dirname($_SERVER['SCRIPT_NAME'])), '/\\'));
+if (!defined('BASE_URL')) {
+    // __DIR__ = C:/xampp/htdocs/Portfolio/config
+    // dirname(__DIR__) = C:/xampp/htdocs/Portfolio 
+    // $_SERVER['DOCUMENT_ROOT'] = C:/xampp/htdocs
+
+    $ruta_proyecto = str_replace('\\', '/', dirname(__DIR__));
+    $ruta_servidor = str_replace('\\', '/', realpath($_SERVER['DOCUMENT_ROOT']));
+
+    $base = str_replace($ruta_servidor, '', $ruta_proyecto);
+
+    define('BASE_URL', rtrim($base, '/'));
+}
